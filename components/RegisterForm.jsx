df4ef8379d9d6a24f4fce5d6c405324580e11e9b
SEERRGEE <ieg.sergo@gmail.com>
2024-01-26 15:28:41 -0500

    create UI for the register page

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,22 +1,23 @@
-import Link from "next/link";
+import Link from "next/link"
 
-export default function LoginForm() {
-    return(
+export default function RegisterForm() {
+    return (
         <div className="grid place-items-center h-screen">
             <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
-                <h1 className='text-lg font-bold my-4'>Login</h1>
+                <h1 className='text-lg font-bold my-4'>Register</h1>
 
                 <form className="flex flex-col gap-3">
+                    <input type={'text'} placeholder='full name'/>
                     <input type={'text'} placeholder='email'/>
                     <input type={'password'} placeholder='password'/>
 
-                    <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">login</button>
+                    <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">register</button>
 
                     <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
                         Error Message
                     </div>
 
-                    <Link className="text-sm mt-3 text-right" href={'/register'}>Don't have an account? <span className="underline">Register</span></Link>
+                    <Link className="text-sm mt-3 text-right" href={'/'}>Already have an account? <span className="underline">Login</span></Link>
                 </form>
             </div>
         </div>
